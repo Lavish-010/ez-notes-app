46abb6f021964faf501ecc5865ded6f3289812e6
LAVISH KUMAR <lavishdama2@gmail.com>
2025-02-15 12:39:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Stopwatch from './components/Stopwatch';
+// ...existing code...
+
+function App() {
+    return (
+        <div className="App">
+            // ...existing code...
+            <Stopwatch />
+            // ...existing code...
+        </div>
+    );
+}
+
+export default App;
